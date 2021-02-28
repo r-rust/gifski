bb6894bb13b843b92552fb3d6334a0d0a70746de
Jeroen Ooms <jeroenooms@gmail.com>
2021-02-28 22:36:06 +0100

    Bump rwinlib

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
 # Build against gifski libs compiled with the Rtools
-if(!file.exists("../windows/gifski-0.8.6/include/gifski.h")){
+if(!file.exists("../windows/gifski-0.8.7/include/gifski.h")){
   if(getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/rwinlib/gifski/archive/v0.8.6.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/gifski/archive/v0.8.7.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
