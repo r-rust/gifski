15bd532bf44bcdcb2784c8e7ef8850e58a5b42a0
Jeroen Ooms <jeroenooms@gmail.com>
2022-04-05 13:11:43 +0200

    Use old libs on Vista

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,9 +1,16 @@
 # Build against gifski libs compiled with the Rtools
 VERSION <- commandArgs(TRUE)
+
+# Hack: latest gifski does not work on Windows Vista
+if(grepl('Windows (Vista|Server 2008)', osVersion)){
+  VERSION <- '1.4.3'
+}
+
 if(!file.exists(sprintf("../windows/gifski-%s/include/gifski.h", VERSION))){
   if(getRversion() < "3.3.0") setInternet2()
   download.file(sprintf("https://github.com/rwinlib/gifski/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
+  file.rename(sprintf("../windows/gifski-%s", VERSION), '../windows/gifski-winlibs')
 }
