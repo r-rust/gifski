3e12ecf767f9bcba02197fa00b75e707cca822ac
Jeroen Ooms <jeroenooms@gmail.com>
2023-07-26 23:46:30 +0200

    Make GNU tar compress better

--- src/myrustlib/vendor-update.sh
+++ src/myrustlib/vendor-update.sh
@@ -2,7 +2,7 @@
 rm -Rf vendor vendor.tar.xz
 cargo vendor
 rm -Rf vendor/windows_*_gnullvm/lib/* vendor/windows_*_msvc/lib/*
-tar -cJ --no-xattrs -f vendor.tar.xz vendor
+XZ_OPT=-9 tar -cJ --no-xattrs -f vendor.tar.xz vendor
 rm -Rf vendor
 
 # Update ../inst/AUTHORS
