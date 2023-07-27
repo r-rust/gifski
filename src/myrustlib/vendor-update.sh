75a84cd161e9ac83f5237821593df1eb3da23872
Jeroen Ooms <jeroenooms@gmail.com>
2023-07-27 14:52:58 +0200

    Tweaks for author script

--- src/myrustlib/vendor-update.sh
+++ src/myrustlib/vendor-update.sh
@@ -4,6 +4,3 @@ cargo vendor
 rm -Rf vendor/windows_*_gnullvm/lib/* vendor/windows_*_msvc/lib/* vendor/windows_i686*/lib/*
 XZ_OPT=-9 tar -cJ --no-xattrs -f vendor.tar.xz vendor
 rm -Rf vendor
-
-# Update ../inst/AUTHORS
-Rscript vendor-authors.R
