2d8074b2c81e65e8c672b4548005efcf1c7754eb
Jeroen Ooms <jeroenooms@gmail.com>
2024-10-04 19:15:12 +0200

    Windows: update static libs to gifski 1.32.0

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,11 +1,11 @@
 if(!file.exists("../windows/libgifski/lib")){
   unlink("../windows", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
-    "https://github.com/r-windows/bundles/releases/download/gifski-1.12.2/gifski-1.12.2-clang-aarch64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/gifski-1.32.0/gifski-1.32.0-clang-aarch64.tar.xz"
   } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
-    "https://github.com/r-windows/bundles/releases/download/gifski-1.12.2/gifski-1.12.2-clang-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/gifski-1.32.0/gifski-1.32.0-clang-x86_64.tar.xz"
   }  else if(getRversion() >= "4.2") {
-    "https://github.com/r-windows/bundles/releases/download/gifski-1.12.2/gifski-1.12.2-ucrt-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/gifski-1.32.0/gifski-1.32.0-ucrt-x86_64.tar.xz"
   } else {
     "https://github.com/r-windows/bundles/releases/download/gifski-1.12.2/gifski-1.12.0-msvcrt.tar.xz"
   }
