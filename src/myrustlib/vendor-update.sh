03149e6345b44d97014f0a7f0fd30a37defb5fbf
Jeroen Ooms <jeroenooms@gmail.com>
2024-10-13 11:39:09 +0200

    Shrink vendor bundle a bit

--- src/myrustlib/vendor-update.sh
+++ src/myrustlib/vendor-update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -Rf vendor vendor.tar.xz
 cargo vendor
-rm -Rf vendor/windows_x86_64_gnullvm/lib/* vendor/windows_*_msvc/lib/* vendor/windows_i686*/lib/*
+rm -Rf vendor/windows_x86_64_gnullvm/lib/* vendor/windows_*_msvc/lib/* vendor/windows_i686*/lib/* vendor/winapi-i686-pc-windows-gnu/lib/*
 XZ_OPT=-9 tar -cJ --no-xattrs -f vendor.tar.xz vendor
 rm -Rf vendor
